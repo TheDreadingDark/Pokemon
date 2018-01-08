b24a0ded75f6743f1d862d917a125eecb3aaf849
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-01-08 11:55:03 -0700

    Added code for the Pokedex on the PokemonController class.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,12 +1,36 @@
 package pokemon.controller;
 
-import pokemon.model.Pokemon;
+import pokemon.model.*;
+import pokemon.view.PokemonFrame;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class PokemonController
 {
 	private List<Pokemon> pokedex;
+	private PokemonFrame appFrame;
+	
+	public PokemonController()
+	{
+		pokedex = new ArrayList<Pokemon>();
+		buildPokedex();
+		
+//		appFrame = new PokemonFrame(this);
+	}
+	
+	private void buildPokedex()
+	{
+		pokedex.add(new Turtwig());
+		pokedex.add(new Grotle());
+		pokedex.add(new Torterra());
+		
+		pokedex.add(new Celebi());
+		
+		pokedex.add(new Kyogre());
+		
+		pokedex.add(new Mewtwo());
+	}
 	
 	public List<Pokemon> getPokedex()
 	{
