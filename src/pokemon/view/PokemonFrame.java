07dc4439ddc958e583e8ceb43ce9aab40be5a93b
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-01-12 12:49:32 -0700

    Added PokemonFrame code.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,27 @@
 package pokemon.view;
 
-public class PokemonFrame
-{
+import pokemon.controller.PokemonController;
+import javax.swing.JFrame;
 
+public class PokemonFrame extends JFrame
+{
+	private PokemonController appController;
+	private PokemonPanel appPanel;
+	
+	public PokemonFrame(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		appPanel = new PokemonPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(500, 500);
+		this.setTitle("Pokemon 2018");
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
